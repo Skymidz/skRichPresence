44883ff3b16df5984e35e0ddb52a24d51293067b
Skymidz <100541047+Skymidz@users.noreply.github.com>
2022-10-08 19:13:17 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,7 @@
+ConfigRichPresence = {}
+ConfigRichPresence.ClientID = 
+ConfigRichPresence.RessourceTimer = 30 -- Actualisation en seconde
+ConfigRichPresence.image = ""
+ConfigRichPresence.Buttons = {
+    { index = 0, name = "DISCORD", url = "" },
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'adamant' --cerulean
+game 'gta5'
+lua54 "yes"
+
+author 'Skymidz#7333'
+
+-- Client
+client_scripts {
+    "client.lua",
+}
+
+-- Shared
+shared_scripts {
+    "config.lua",
+}
+
+escrow_ignore {
+    "config.lua",
+    "client.lua"
+}
+  
